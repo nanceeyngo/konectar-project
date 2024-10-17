e4104b74d808121c1c03c2d0987fbdb67f7037eb
Ngozi Philip <nanceeyngo@gmail.com>
2024-10-17 18:44:28 +0100

    m

--- src/homeComponents/ProcessSection.jsx
+++ src/homeComponents/ProcessSection.jsx
@@ -4,11 +4,8 @@ import box from '../assets/images/BOX.png'
 const ProcessSection = () => {
   return (
     <>
-         <div className='py-16'>
-            <h1 className='text-[#003333] font-Manrope font-bold text-center'>Our Process: The Golden Path</h1>    
-        </div>   
 
-        <div className='m-auto md:px-20 w-[80%]'>
+        <div className='m-auto md:px-20 w-[80%] py-16'>
             <img src={box} alt="Our Process: The Golden Path" />
         </div>
         </>
