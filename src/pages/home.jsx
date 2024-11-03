827ce47988f6c0e592b8d9c8161472d56bf934f2
Ngozi Philip <nanceeyngo@gmail.com>
2024-11-03 18:43:51 +0100

    new

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import MissionSection from "../homeComponents/MissionSection";
 import HeroSection from "../homeComponents/HeroSection";
@@ -15,6 +15,28 @@ import OurVisionSection from '../homeComponents/OurVisionSection';
 import ProductsSection from '../homeComponents/ProductsSection';
 
 const Home = () => {
+
+    const [showButton, setShowButton] = useState(false);
+
+// Function to scroll to the top
+const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  // Show the button when scrolled down
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
     return (
         <>
     
@@ -31,6 +53,16 @@ const Home = () => {
     
     <FAQSection />
     <TeamSection />
+       
+       {/* Scroll to Top Button */}
+      {showButton && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-8 right-8 bg-[#009933] text-white p-3 rounded-full shadow-lg hover:bg-[#003311] transition duration-300"
+        >
+          Back to Top ↑
+        </button>
+      )}
         </>
     );
 };
